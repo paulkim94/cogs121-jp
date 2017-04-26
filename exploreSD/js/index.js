6161d232830093cc24a3130a7ab4b847356c0cd1
Judd Gatbonton <juddgatbonton@Judds-MacBook-Pro-2.local>
2017-04-25 18:56:32 -0700

    adding SD map for first core map interaction

---
+++ exploreSD/js/index.js
@@ -0,0 +1,10 @@
+// initializes map and sets center coordinates and zoom level
+var map = L.map('map').setView([32.716, -117.161], 13);
+
+// mapbox tile layer, can also adjust max zoom level
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamdhdGJvbnRvbiIsImEiOiJjajF2ZTFoYXMwMDA5MzJtdzZpNjN3dTZnIn0._REegf1q-yPULYDHXmQNeQ', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'your.mapbox.project.id',
+    accessToken: 'your.mapbox.public.access.token'
+}).addTo(map);
