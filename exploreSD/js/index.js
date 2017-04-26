09f2939e2364d33d0a4949097ce8b337ec63ad38
Paul Kim <pak013@ucsd.edu>
2017-04-25 22:38:40 -0700

    Pushed milestone

--- exploreSD/js/index.js
+++ exploreSD/js/index.js
@@ -1,5 +1,5 @@
 // initializes map and sets center coordinates and zoom level
-var map = L.map('map').setView([32.716, -117.161], 13);
+var map = L.map('map').setView([32.716, -117.161], 11);
 
 // mapbox tile layer, can also adjust max zoom level
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamdhdGJvbnRvbiIsImEiOiJjajF2ZTFoYXMwMDA5MzJtdzZpNjN3dTZnIn0._REegf1q-yPULYDHXmQNeQ', {
@@ -8,3 +8,23 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x
     id: 'your.mapbox.project.id',
     accessToken: 'your.mapbox.public.access.token'
 }).addTo(map);
+
+var seaWorldMarker = L.marker([32.764107, -117.226265]).addTo(map);
+var ucsdMarker = L.marker([32.876093, -117.235436]).addTo(map);
+var balboaParkMarker = L.marker([32.734148, -117.144553]).addTo(map);
+
+seaWorldMarker.bindPopup("<b>Sea World</b><br><u>Description</u><br> Sea World San Diego is an animal theme park, oceanarium, outside aquarium, and marine mammal park, located in San Diego, California, United States. The park is owned by SeaWorld Entertainment.<br>"
++ "<u>Price</u><br> $50+ ");
+
+ucsdMarker.bindPopup("<b>UCSD</b><br><u>Description</u><br>" +
+"The University of California, San Diego is a public research university located in the La Jolla neighborhood of San Diego, California, in the United States.<br>" +
+"<u>Price</u><br>Free ");
+
+balboaParkMarker.bindPopup("<b>Balboa Park</b><br><u>Description</u><br>" +
+"Balboa Park is a 1,200-acre urban cultural park in San Diego, California, United States.<br>" +
+"<u>Price</u><br>Free ");
+
+var popup = L.popup()
+    .setLatLng(latlng)
+    .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+    .openOn(map);
---
+++ milestone5.md
@@ -0,0 +1,28 @@
+Work Done So Far
+
+Paul Kim
+- Created storyboards for two different ideas that our team discussed during the ideation/brainstorm phase
+- Created a paper prototype for the Travel Guide application our team decided to go with
+- Completed the Quick Start Leaflet tutorial that shows how to set up the map and add basic UI components to it to get familiar with it for our application
+- Completed another Leaflet tutorial that shows how to make a choropleth map
+- Messed around with the Google Maps API to see if it's a good fit for our application
+- Added markers of a few popular places/attractions in San Diego to our map that show information about that place
+
+Judd Gatbonton
+- Created storyboards and paper prototypes
+- Familiarized himself with the Leaflet API and Google Maps API through their online tutorials (Quick Start and Choropleth tutorial for Leaflet & the Quick Start tutorial for Google Maps API)
+- Created the first iteration for the San Diego map for our first core map interaction
+
+Ranvijay Singh
+-
+
+
+Anthony Grundle
+- Created storyboards and paper prototypes
+- Completed Leaflet tutorial on making a choropleth map
+- Did research on possible APIs to pull data from for our application such as Google Places and SkyScanner
+
+Map Screenshot
+![Map Screenshot](/exploreSD/exploreSD.jpg?raw=true)
+
+The screenshot above is showing our application's map and how the markers display information about a place in SD when the user clicks on it.
