2ecf192796242629d567cdef10c718763504d793
Judd Gatbonton <juddgatbonton@MacBook-Pro.local>
2017-05-01 11:07:41 -0700

    added markers for museums and stadiums without descriptions

--- exploreSD/js/index.js
+++ exploreSD/js/index.js
@@ -9,6 +9,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x
     accessToken: 'your.mapbox.public.access.token'
 }).addTo(map);
 
+// markers for college campuses and landmarks
 var seaWorldMarker = L.marker([32.764107, -117.226265]).addTo(map);
 var ucsdMarker = L.marker([32.876093, -117.235436]).addTo(map);
 var balboaParkMarker = L.marker([32.734148, -117.144553]).addTo(map);
@@ -33,7 +34,7 @@ var SanDiegoCaliforniaTempleMarker = L.marker([32.866400, -117.228800]).addTo(ma
 var SalkInstituteMarker = L.marker([32.887047, -117.244785]).addTo(map);
 
 
-seaWorldMarker.bindPopup("<b>Sea World</b><br><u>Description</u><br> Sea World San Diego is an animal theme park, oceanarium, outside aquarium, and marine mammal park, located in San Diego, California, United States. The park is owned by SeaWorld Entertainment.<br>" + 
+seaWorldMarker.bindPopup("<b>Sea World</b><br><u>Description</u><br> Sea World San Diego is an animal theme park, oceanarium, outside aquarium, and marine mammal park, located in San Diego, California, United States. The park is owned by SeaWorld Entertainment.<br>" +
 "<u>Price</u><br> $50+ ");
 
 ucsdMarker.bindPopup("<b>UCSD</b><br><u>Description</u><br>" +
@@ -123,28 +124,54 @@ SalkInstituteMarker.bindPopup("<b>Salk Institute for Biological Studies</b><br><
 
 
 
+// markers for museums and stadiums
+var ussMidwayMuseumMarker = L.marker([32.7137, -117.1751]).addTo(map);
+var natHistMuseumMarker = L.marker([32.7321, -117.1475]).addTo(map);
+var museumOfManMarker = L.marker([32.7318, -117.1523]).addTo(map);
+var airSpaceMuseumMarker = L.marker([32.7263, -117.1543]).addTo(map);
+var modelRailroadMuseumMarker = L.marker([32.7311, -117.1486]).addTo(map);
+var photoArtsMuseumMarker = L.marker([32.7311, -117.1488]).addTo(map);
+var musContArtSDMarker = L.marker([32.84444, -117.27806]).addTo(map);
+var mingeiIntMuseumMarker = L.marker([32.7311, -117.1510]).addTo(map);
+var maritimeMuseumMarker = L.marker([32.7209, -117.1740]).addTo(map);
+var museumOfArtMarker = L.marker([32.7322, -117.1504]).addTo(map);
+var automativeMuseumMarker = L.marker([32.7275, -117.1539]).addTo(map);
+var newChildrenMuseumMarker = L.marker([32.7106, -117.1652]).addTo(map);
+var starOfIndiaMarker = L.marker([32.7203, -117.1736]).addTo(map);
+var qualcommMuseumMarker = L.marker([32.897290, -117.195714]).addTo(map);
+var birchAquariumScrippsMarker = L.marker([32.8678, -117.2496]).addTo(map);
+var fleetScienceCenterMarker = L.marker([32.7308, -117.1470]).addTo(map);
+var wellsFargoHistMuseumMarker = L.marker([32.7541, -117.1973]).addTo(map);
 
+var qualcommStadiumMarker = L.marker([32.7831, -117.1196]).addTo(map);
+var petcoParkMarker = L.marker([32.7072, -117.1571]).addTo(map);
+var delMarRaceTrackMarker = L.marker([32.9757, -117.2614]).addTo(map);
+var valleyViewCenterMarker = L.marker([32.7553, -117.2122]).addTo(map);
 
+ussMidwayMuseumMarker.bindPopup("<b>USS Midway Museum</b>");
+natHistMuseumMarker.bindPopup("<b>San Diego Natural History Museum</b>");
+museumOfManMarker.bindPopup("<b>San Diego Museum of Man</b>");
+airSpaceMuseumMarker.bindPopup("<b>San Diego Air & Space Museum</b>");
+modelRailroadMuseumMarker.bindPopup("<b>San Diego Model Railroad Museum</b>");
+photoArtsMuseumMarker.bindPopup("<b>Museum of Photographic Arts</b>");
+musContArtSDMarker.bindPopup("<b>Museum of Contemporary Art San Diego</b>");
+mingeiIntMuseumMarker.bindPopup("<b>Mingei International Museum</b>");
+maritimeMuseumMarker.bindPopup("<b>Maritime Museum of San Diego</b>");
+museumOfArtMarker.bindPopup("<b>San Diego Museum of Art</b>");
+automativeMuseumMarker.bindPopup("<b>San Diego Automative Museum</b>");
+newChildrenMuseumMarker.bindPopup("<b>The New Children's Museum</b>");
+starOfIndiaMarker.bindPopup("<b>Star of India</b>");
+qualcommMuseumMarker.bindPopup("<b>Qualcomm Museum</b>");
+birchAquariumScrippsMarker.bindPopup("<b>Birch Aquarium at Scripps</b>");
+fleetScienceCenterMarker.bindPopup("<b>Fleet Science Center</b>");
+wellsFargoHistMuseumMarker.bindPopup("<b>Wells Fargo History Museum</b>");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+qualcommStadiumMarker.bindPopup("<b>Qualcomm Stadium</b><br><u>Description</u><br>" +
+"The Salk Institute for Biological Studies is an independent, non-profit, scientific research institute located in La Jolla, San Diego, California, United States.<br>" +
+"<u>Price</u><br>Free ");
+petcoParkMarker.bindPopup("<b>Petco Park</b>");
+delMarRaceTrackMarker.bindPopup("<b>Del Mar Racetrack</b>");
+valleyViewCenterMarker.bindPopup("<b>Valley View Casino Center</b>");
 
 
 
