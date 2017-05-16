5c698abcb167f842fbb55e04f2efc18a78e932f1
Paul Kim <pak013@ucsd.edu>
2017-05-15 18:11:03 -0700

    Added tabs in sidebar

--- exploreSD/public/js/index.js
+++ exploreSD/public/js/index.js
@@ -228,3 +228,18 @@ function initializeMapMarkers(locationsArray) {
   displayed = L.layerGroup(displayedmarkers);
   displayed.addTo(map);
 }
+
+// Open tab function
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
