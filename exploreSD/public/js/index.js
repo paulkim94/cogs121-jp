9ec496bbce2b45ddcf9380df0e042780694c42d3
Paul Kim <pak013@ucsd.edu>
2017-05-19 17:36:29 -0700

    Displays list of places without hiding it initially

--- exploreSD/public/js/index.js
+++ exploreSD/public/js/index.js
@@ -268,11 +268,11 @@ function initializeMapMarkers(locationsArray) {
 // Open tab function
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = $(".tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = $(".tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
